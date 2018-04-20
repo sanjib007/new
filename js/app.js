8b92491c33ac42b07529f9bc612505674ed60359
sanjib007 <loton1984@gmail.com>
2018-04-20 20:28:34 +0600

    abcd

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $("[rel='tooltip']").tooltip();
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').fadeOut(205)
+        }
+    );
+});
