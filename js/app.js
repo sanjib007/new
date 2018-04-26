d0fee983c6aa2937fd37f2752ca6c46621e9c21f
tapos <tapos.aa@gmail.com>
2018-04-26 23:32:19 +0600

    commit sanjib work

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
 $( document ).ready(function() {
-    $("[rel='tooltip']").tooltip();
 
-    $('.thumbnail').hover(
-        function(){
-            $(this).find('.caption').fadeIn(250)
-        },
-        function(){
-            $(this).find('.caption').fadeOut(205)
+        //do something
+
+        //  $(".btn-round").click({animateIn: "closeButton", animateOut: "plusButton"}, animate_function);
+        $(".btn-square").click({animateIn: "circleShape", animateOut: "squareShape"}, animate_function);
+
+
+        function animate_function(event){
+            var info = $(this);
+            var sectionDIv  = info.closest('.sectionDiv');
+            sectionDIv.toggleClass('bg_red');
+            sectionDIv.find(".innerText").toggle();
+            sectionDIv.closest('.row').find('.rightshowDiv').toggle();
+            sectionDIv.closest('.row').find('.blackBox').toggle();
+            if( $(this).hasClass(event.data.animateIn) ) {
+
+                $(this).removeClass(event.data.animateIn).addClass(event.data.animateOut);
+            }
+            else if( $(this).hasClass(event.data.animateOut) ) {
+
+
+                $(this).removeClass(event.data.animateOut).addClass(event.data.animateIn);
+
+            }
+            else {
+
+
+                $(this).addClass('animated ' + event.data.animateIn);
+            }
         }
-    );
-
-
-
-
 
 });
